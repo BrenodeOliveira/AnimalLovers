f8eb952f04536d27bb3f655dc2276511ce6f6c68
Breno <brenorocha947@gmail.com>
2020-10-15 19:56:49 -0300

    Tela de login Layout

--- app/src/main/java/br/com/breno/animallovers/ui/activity/LoginActivity.kt
+++ app/src/main/java/br/com/breno/animallovers/ui/activity/LoginActivity.kt
@@ -1,12 +1,18 @@
 package br.com.breno.animallovers.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.breno.animallovers.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btn_criar_conta_login.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }
